12c229b250f7e5edcf7b28f75e85286b73c43205
Ted Zhu <tedz2usa@gmail.com>
2016-08-17 11:27:07 -0400

    Remove unnecessary comments.

--- assist_summary.php
+++ assist_summary.php
@@ -1,9 +1,3 @@
-	<!-- <html>
-<head>
-		<meta charset="UTF-8">
-		<title>Title.</title> -->
-		<!-- <link rel="stylesheet" href="assist1_mod.css"> -->
-
 <?php
 
 	session_start();
@@ -25,24 +19,7 @@
 		$operator2Style = ' ';
 	}
 
-?>
-<!-- 	
-</head>
-
-<body>
-	
-	<div id="page">
-		<img src="rail.jpg" alt=" " style="position:absolute; top:0px; left:0px; width:1300px; height:600px; opacity:0.15;"/>
-		<div id="top_panel"></div>
-		
-		<div id="title">
-				<h1>Simulator of Human Operator Workload (SHOW)<img src="hal.png" width=400 align="middle" /> </h1>
-		</div>
-		
-		<br><br>
-		 -->
-		 
-	
+?>	
 	
 	<style>		
 		#low_work_0, #high_work_0{ color: <?php if(($low_count_0+$high_count_0)>$normal_count_0){ echo "red";} else{ echo "black";}?>;}
@@ -52,9 +29,8 @@
 		#normal_work_1{ color: <?php if(($low_count_1+$high_count_1)<$normal_count_1){ echo "green";} else{ echo "black";}?>;}
 	</style>
 	
-<?php
-		require_once("assist.html");
-?>
+<?php require_once("assist.html"); ?>
+
 	<br>
 	<br>
 	<br>
@@ -74,9 +50,4 @@
 		<button id="back_button" onclick="location.href='replications.php'">Peek & Tweak, under the hood</button>
 	</div>
 	
-<?php
-	require_once('footer.php');
-?>
-<!-- </body>	
-</html>
- -->
+<?php require_once('footer.php'); ?>
