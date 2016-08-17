54e1f9629bf857f10630b43640b57679b56a5049
branchvincent <branchevincent@gmail.com>
2016-08-17 12:19:18 -0400

    Moved style sheets to styeles folder

--- header.php
+++ header.php
@@ -2,21 +2,21 @@
 <head>
 		<meta charset="UTF-8">
 		<title>Title.</title>
-		<link rel="stylesheet" href="global_styles.css.php">
+		<link rel="stylesheet" href="styles/global_styles.css.php">
 		<script type="text/javascript" src="sim_settings_entry.js"></script>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 		<?php
-			echo $html_head_insertions; 
+			echo $html_head_insertions;
 		?>
-		
+
 </head>
 
 <body>
-	
+
 	<div id="fixedHead">
-		
+
 		<div id="top_panel"></div>
-		
+
 		<div id="title">
 				<h1>Simulator of Human Operator Workload (SHOW)</h1>
 		</div>
@@ -33,4 +33,3 @@
 
 
 	<div id="main">
-	
