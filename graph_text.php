9271c84471e7e50839dc517677dec0ec6409a708
branchvincent <branchevincent@gmail.com>
2016-08-17 12:07:36 -0400

    Moved data files (csv, txt) to sesssions folder

--- graph_text.php
+++ graph_text.php
@@ -1,12 +1,12 @@
 <?php
-	
-	
-	$file_handle=fopen('Engineer_stats.csv','r');
+
+
+	$file_handle=fopen('sessions/Engineer_stats.csv','r');
 	$count=array();
 	$temp_count=0;
 	$skip=1;
 	$num=0;
-	while (! feof($file_handle) ) 
+	while (! feof($file_handle) )
 	{
 		if($temp_count==1)
 		{
@@ -22,7 +22,7 @@
 			$num=count($line_of_text);
 			$count[$temp_count]=array();
 			for($i=1;$i<$num;$i++)
-			{	
+			{
 				if($temp_count==0)
 				{
 					$count[$temp_count][$i-1]=$line_of_text[$i];
@@ -31,7 +31,7 @@
 				{
 					$count[$temp_count][$i-1]=(float)$line_of_text[$i];
 				}
-				
+
 			}
 			$skip=0;
 			$temp_count++;
@@ -42,28 +42,28 @@
 			$skip=1;
 			continue;
 		}
-		
+
 	}
-	
+
 	fclose($file_handle);
-	
+
 	$type_byPhase1=array();
 	$type_byPhase2=array();
 	$type_byPhase3=array();
-	
-	
+
+
 	for($j=1;$j<$temp_count-1;$j++)
 	{
-		
+
 		$type_byPhase1[$j]=array();
 		$type_byPhase2[$j]=array();
 		$type_byPhase3[$j]=array();
 		for($i=2;$i<$num;$i++)
 		{
-			
+
 			if($i<5){
 				$type_byPhase1[$j][]=$count[$j][$i];
-				
+
 				/* array_push($type_byPhase1[$j], $count[$j][$i]); */
 			}
 			else{
@@ -77,19 +77,19 @@
 			}
 		}
 	}
-	
-	
-	
+
+
+
 	$count_type_high1=array();
 	$count_type_high2=array();
 	$count_type_high3=array();
 	$count_type_low1=array();
 	$count_type_low2=array();
 	$count_type_low3=array();
-	
+
 	$count_type_low=array();
 	$count_type_high=array();
-	
+
 	for($j=1;$j<$temp_count-1;$j++){
 		$count_type_high1[$j]=0;
 		$count_type_low1[$j]=0;
@@ -97,13 +97,13 @@
 		$count_type_low2[$j]=0;
 		$count_type_high3[$j]=0;
 		$count_type_low3[$j]=0;
-		
+
 		$count_type_high[$j]=0;
 		$count_type_low[$j]=0;
 	}
-		
+
 	for($i=2;$i<$num;$i++)
-	{		
+	{
 		for($j=1;$j<$temp_count-1;$j++)
 		{
 			if($count[10][$i]>0.7)
@@ -132,13 +132,13 @@
 						}
 					}
 				}
-				continue;				
+				continue;
 			}
-			
+
 			if($count[10][$i]<0.3)
 			{
 				if($i<5)
-				{	
+				{
 					if($count[$j][$i]<(array_sum($type_byPhase1[$j])/count($type_byPhase1[$j])))
 					{
 						$count_type_low1[$j]++;
@@ -161,14 +161,14 @@
 						}
 					}
 				}
-				continue;				
+				continue;
 			}
-			
-			
-			
+
+
+
 		}
 	}
-	
+
 	arsort($count_type_low);
 	arsort($count_type_high);
 	print_r($count_type_high);
@@ -176,10 +176,10 @@
 
 <html>
 <body>
-	
+
 	<div id="text_box">
 		<h3>These combined factors contributed to period of high workload: </h3>
-		<ul> 
+		<ul>
 <?php
 	$high_keys=array_keys($count_type_high);
 	for($j=1;$j<$temp_count-1;$j++)
@@ -208,14 +208,14 @@
 			}
 
 		}
-		
-		
+
+
 	}
 	echo "</ul>" ;
 ?>
 		<br><br><br>
 		<h3>These combined factors contributed to period of low workload: </h3>
-		<ul> 
+		<ul>
 
 <?php
 	$low_keys=array_keys($count_type_low);
@@ -245,20 +245,20 @@
 			}
 
 		}
-		
-		
+
+
 	}
 	echo "</ul>" ;
-?>		
-		
+?>
+
 
 </div>
-		
+
 </body>
 </html>
-	
-		
-<?php 
+
+
+<?php
  echo "<style>";
 
  for($j=1;$j<$temp_count-1;$j++)
@@ -268,10 +268,10 @@
 			echo "#low". ($low_keys[$j-1]-1)."{ display: none;}";
 		}
 	}
- 
+
  echo "</style>";
- 
-?>			
+
+?>
 
 <script>
 
@@ -284,12 +284,10 @@ for($j=1;$j<$temp_count-1;$j++)
 		function display<?php echo ($low_keys[$j-1]-1) ;?>(){
 			if(document.getElementById(<?php echo 'low='. ($low_keys[$j-1]-1); ?>).style.display=='none')
 			{
-				
+
 			}
 		}
 
 
 
 <script>
-
-	
