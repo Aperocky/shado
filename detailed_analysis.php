c3639222e0f52770826bbcd85fe87ac0167c2ea9
akshit1223 <budhrajaakshit@gmail.com>
2016-08-19 20:37:09 +0530

    detailed analysis page added

---
+++ detailed_analysis.php
@@ -0,0 +1,37 @@
+<?php
+
+	session_start();
+	require_once("side_navigation.html");
+	require_once('header.php');
+	
+	if ($_SESSION['operator1'] == -1) {
+		$operator2Style = 'display:none; ';
+	} else {
+		$operator2Style = ' ';
+	}
+?>
+
+<?php
+			
+		require_once("graph_engineer.php");
+		require_once("graph_text_engineer.php");
+		
+		$assistant= $_SESSION['operator1'];
+		if($assistant==1)
+		{
+			require_once("graph_conductor.php");
+			require_once("graph_text_conductor.php");
+		}
+?>
+
+
+<?php
+	require_once("footer.php");
+?>
+
+<style>
+#resultAnalysis{
+font-weight: bold;
+background-color: 00B1FF;
+}
+</style>
